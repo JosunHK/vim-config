6d98bdd2853ebd0950472d2cf3949ea607765c47
dennistai.mail@gmail.com <dennistai.mail@gmail.com>
2024-07-29 01:32:48 +0800

    FIX init.lua

--- lua/josun/init.lua
+++ lua/josun/init.lua
@@ -24,7 +24,6 @@ local function my_on_attach(bufnr)
     -- custom mappings
     vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
     vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-    vim.keymap.set('n', 'mm', api.fs.create({ node = vim.fn.input("node > ") }))
 end
 
 require("nvim-tree").setup({
