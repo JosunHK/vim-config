caa821119734f87c87344dca83c1d2c4d43e3ac4
dennistai.mail@gmail.com <dennistai.mail@gmail.com>
2024-07-29 02:12:00 +0800

    quick kill telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,5 +2,16 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")});
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+local actions = require("telescope.actions")
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+})
